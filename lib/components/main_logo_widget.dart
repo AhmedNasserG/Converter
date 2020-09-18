ce38c230b6890ca4fb682c0d7d65cacc2b381fbc
Ahmed-Nasser <ahmednasser217217@gmail.com>
2020-09-18 02:53:20 +0200

    add:animated text to the main logo
    complete animation

--- lib/components/main_logo_widget.dart
+++ lib/components/main_logo_widget.dart
@@ -1,6 +1,8 @@
 import 'package:convertor/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class MainLogoWidget extends StatelessWidget {
   @override
@@ -23,14 +25,39 @@ class MainLogoWidget extends StatelessWidget {
           SizedBox(
             width: 10.0,
           ),
-          Text(
-            'Converter',
-            style: TextStyle(
-              fontSize: logoSize,
-              color: Colors.white,
-            ),
-          ),
+          LogoAnimatedText(),
         ],
+      ),
+    );
+  }
+}
+
+class LogoAnimatedText extends StatefulWidget {
+  @override
+  _LogoAnimatedTextState createState() => _LogoAnimatedTextState();
+}
+
+class _LogoAnimatedTextState extends State<LogoAnimatedText> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 250.0,
+      child: ColorizeAnimatedTextKit(
+        text: [
+          "Converter",
+          // "Anything",
+          // "Now",
+        ],
+        textStyle: TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900),
+        colors: [
+          Colors.white,
+          Colors.grey,
+          Colors.blueGrey,
+          Colors.black12,
+        ],
+        textAlign: TextAlign.center,
+        alignment: AlignmentDirectional.topStart, // or Alignment.topLeft
+        totalRepeatCount: 1,
       ),
     );
   }
